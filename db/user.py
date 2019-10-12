553bea3e9d5966910f909fdfd4e6cc0368905181
zpg <374652530@qq.com>
2019-10-12 17:10:32 +0800

    zpg

--- db/user.py
+++ db/user.py
@@ -1,6 +1,6 @@
 import pymysql
 def into_register_info(username,password):
-    db = pymysql.connect("127.0.0.1", "root", "123456", "library")
+    db = pymysql.connect("47.96.139.19", "root", "123456", "library")
     cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
     try:
         sql = "select count(user_account) as count from user where user_account = '{}'".format(username)
