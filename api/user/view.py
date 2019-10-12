2864aaceed32f2fa718eaf7ffb7bd2e67084cff7
zpg <374652530@qq.com>
2019-10-12 16:11:14 +0800

    zpg

--- api/user/view.py
+++ api/user/view.py
@@ -1,7 +1,9 @@
-from flask import Blueprint,jsonify,request
+from flask import Blueprint,jsonify,request,session,escape
 from models.user import UserRegister
 from db.user import into_register_info,user_login
-import json
+from flask_mail import Mail,Message
+import json,random
+from models.emailverify import dict_Verify
 
 user = Blueprint("user",__name__)
 
@@ -10,9 +12,13 @@ def user_verify_register():
     data = json.loads(request.get_data("").decode("utf-8"))
     username = data["username"]
     password = data["password"]
+    verifycode = data["verifycode"]
+    email = data["email"]
     user = UserRegister(username,password)
     password_result = user.password
     username_result = user.username
+    if email and verifycode != dict_Verify[email]:
+        return jsonify({"status": -1, "message": "验证码错误"})
     if not username_result[0]:
         return jsonify({"status": -1, "message": username_result[1]})
     if not password_result[0]:
@@ -21,6 +27,22 @@ def user_verify_register():
     if not into_resutl[0]:
         return jsonify({"status": -1, "message": into_resutl[1]})
     return jsonify({"status": 0, "message": into_resutl[1]})
+
+@user.route("email_verify",methods = ["post"])
+def email_verify():
+    try:
+        data = json.loads(request.get_data("").decode("utf-8"))
+        email = data["email"]
+        verifycode = str(random.randint(100000,999999))
+        mail = Mail()
+        message = Message(subject="验证码",
+                          recipients=[email],
+                          body=verifycode)
+        dict_Verify[email] = verifycode
+        mail.send(message)
+        return jsonify({"status": 0, "message": "验证码发送成功"})
+    except:
+        return jsonify({"status": -1, "message": "验证码发送失败"})
 
 
 @user.route("login",methods = ["post"])
@@ -38,7 +60,20 @@ def user_verify_login():
     into_resutl = user_login(username_result[1],password_result[1])
     if not into_resutl[0]:
         return jsonify({"status": -1, "message": into_resutl[1]})
-    return jsonify({"status": 0, "message": into_resutl[1]})
+    if "username"in session :
+        return jsonify({"status": -1, "message": "用户已经登入"})
+    session["username"] = username
+    return jsonify({"status": 0, "message": into_resutl[1],"data":escape(session['username'])})
+
+@user.route("logout", methods = ["post"])
+def logout():
+    session.pop("username",None)
+    return jsonify({"status": 0, "message": "退出成功"})
+
+
+
+
+
 
 
 @user.route("UpdatePassword",methods = ["post"])
--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from config import mail_config
+
+from flask_mail import Mail
+from flask import Flask,session
 from flask_cors import *
 import api
+import os
+from datetime import timedelta
+
+# import os
+# path = os.path.dirname(__file__)
+
+mail = Mail()
 
 app = Flask(__name__)
+app.config.from_object(mail_config)
+mail.init_app(app)
+
+app.config['SECRET_KEY']=os.urandom(24)   #设置为24位的字符,每次运行服务器都是不同的，所以服务器启动一次上次的session就清除。
+app.config['PERMANENT_SESSION_LIFETIME']=timedelta(days=7) #设置session的保存时间。
 
 app.register_blueprint(api.user.view.user, url_prefix ="/user")
 CORS(app, supports_credentials=True)
