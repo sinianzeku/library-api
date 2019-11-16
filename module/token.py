2a3abe8a1cb2f14a15ba37ce1c8488f14edfca8c
zpg <374652530@qq.com>
2019-11-16 10:09:25 +0800

    zpg

--- module/token.py
+++ module/token.py
@@ -15,21 +15,21 @@ def generate_token(key, expire=60*60*24*7):
 
 # 验证token 入参：用户id 和 token
 def certify_token(key, token):
-    token_str = base64.urlsafe_b64decode(token).decode('utf-8')
-    token_list = token_str.split(':')
-    if len(token_list) != 2:
+    try:
+        token_str = base64.urlsafe_b64decode(token).decode('utf-8')
+        token_list = token_str.split(':')
+        if len(token_list) != 2:
+            return False
+        ts_str = token_list[0]
+        if float(ts_str) < time.time():
+            # token expired
+            return False
+        known_sha1_tsstr = token_list[1]
+        sha1 = hmac.new(key.encode("utf-8"), ts_str.encode('utf-8'), 'sha1')
+        calc_sha1_tsstr = sha1.hexdigest()
+        if calc_sha1_tsstr != known_sha1_tsstr:
+            return False
+        return True
+    except:
         return False
-    ts_str = token_list[0]
-    if float(ts_str) < time.time():
-        # token expired
-        return False
-    known_sha1_tsstr = token_list[1]
-    sha1 = hmac.new(key.encode("utf-8"), ts_str.encode('utf-8'), 'sha1')
-    calc_sha1_tsstr = sha1.hexdigest()
-    if calc_sha1_tsstr != known_sha1_tsstr:
-        return False
-    return True
-
-
-
 
--- user_activity/activity_private/view.py
+++ user_activity/activity_private/view.py
@@ -12,7 +12,7 @@ user_activity = Blueprint("activity_private",__name__)
 def count_book():
     data = json.loads(request.get_data("").decode("utf-8"))
     if not data["user_account"]:
-        return jsonify({"status": -1, "message": "fail", "data":[]})
+        return jsonify({"status": -1, "message": "fail", "data": []})
     account = data["user_account"]
     tokens = data["tokens"]
     if not token.certify_token(account, tokens):
@@ -24,51 +24,55 @@ def count_book():
 
 
 #借书记录
-@user_activity.route("borrowed_records", methods = ["post"])
+@user_activity.route("borrowed_records", methods=["post"])
 def borrowed_records():
     data = json.loads(request.get_data("").decode("utf-8"))
     user_account = data["user_account"]
     user_id = sql_query_user_id(user_account)
     result = db_user_activity.sql_borrowed_records(user_id)
     if not result[0]:
-        return jsonify({"status":-1,"message":result[1]})
-    return jsonify({"status":0,"message":"success","data":result[1]})
+        return jsonify({"status": -1, "message": result[1]})
+    return jsonify({"status": 0, "message": "success", "data": result[1]})
 
-#在借书籍
-@user_activity.route("borrowing_books", methods = ["post"])
+
+# 在借书籍
+@user_activity.route("borrowing_books", methods=["post"])
 def borrowing_books():
     data = json.loads(request.get_data("").decode("utf-8"))
     user_account = data["user_account"]
     user_id = sql_query_user_id(user_account)
     result = db_user_activity.sql_borrowing_books(user_id)
     if not result[0]:
-        return jsonify({"status":-1,"message":result[1]})
-    return jsonify({"status":0,"message":"success","data":result[1]})
+        return jsonify({"status": -1, "message": result[1]})
+    return jsonify({"status": 0, "message": "success", "data": result[1]})
 
-#我的书架
-@user_activity.route("my_bookshelf", methods = ["post"])
+
+# 我的书架
+@user_activity.route("my_bookshelf", methods=["post"])
 def my_bookshelf():
     data = json.loads(request.get_data("").decode("utf-8"))
     user_account = data["user_account"]
     user_id = sql_query_user_id(user_account)
     result = db_user_activity.sql_my_bookshelf(user_id)
     if not result[0]:
-        return jsonify({"status":-1,"message":result[1]})
-    return jsonify({"status":0,"message":"success","data":result[1]})
+        return jsonify({"status": -1, "message": result[1]})
+    return jsonify({"status": 0, "message": "success", "data": result[1]})
 
-#收藏书籍
-@user_activity.route("collect_book",methods = ["post"])
+
+# 收藏书籍
+@user_activity.route("collect_book", methods=["post"])
 def collect_book():
     data = json.loads(request.get_data("").decode("utf-8"))
     book_id = data["book_id"]
     user_name = data["user_name"]
-    result = db_user_activity.sql_collect_book(user_name,book_id)
+    result = db_user_activity.sql_collect_book(user_name, book_id)
     if not result[0]:
-        return jsonify({"status":-1,"message":result[1]})
-    return jsonify({"status":0,"message":"success"})
+        return jsonify({"status": -1, "message": result[1]})
+    return jsonify({"status": 0, "message": "success"})
 
-#三行情书活动
-@user_activity.route("thematic_activities",methods=["post"])
+
+# 三行情书活动
+@user_activity.route("thematic_activities", methods=["post"])
 def thematic_activities():
     data = json.loads(request.get_data("").decode("utf-8"))
     user_name = data["user_name"]
@@ -78,7 +82,7 @@ def thematic_activities():
     email = data["email"]
     result = db_user_activity.sql_thematic_activities(contestant,phone,works,user_name)
     if not result[0]:
-        return jsonify({"status":-1,"message":result[1]})
+        return jsonify({"status": -1, "message":result[1]})
     subject="图书馆活动报名",
     body='您已成功报名图书馆‘三行情书’活动'
     sendinfo(subject,email,body)
